9a8c30fc160aef3817eb705d3280246a4f099029
Stefanus Adrian <stefanusj06@gmail.com>
2023-11-07 17:07:59 +0700

    finish exercise 02_repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
+    if (num < 0) {
+        return "ERROR";
+    }
+
+    let stringRepeated = "";
+
+    for (let i = 0; i < num; i++) {
+        stringRepeated += string;
+    }
+    
+    return stringRepeated;
 };
 
 // Do not edit below this line
