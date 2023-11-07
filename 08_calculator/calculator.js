0caaad03c0273866f1b8d977bc689f5b5d2aacd5
Stefanus Adrian <stefanusj06@gmail.com>
2023-11-07 19:13:46 +0700

    finish exercise 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sumTotal = 0;
+  array.forEach(value => sumTotal += value);
+  return sumTotal;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let multiplyTotal = 1;
+  array.forEach(value => multiplyTotal *= value);
+  return multiplyTotal;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+  return base ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(factor) {
+
+  if (factor === 1 || factor === 0) {
+    return 1;
+  }
+
+  return factor * factorial(factor - 1);
 };
 
 // Do not edit below this line
